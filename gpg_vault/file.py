13d2701a9c281d77d575c7118a2ef2d891835c58
Paul Taylor <pftylr@gmail.com>
2018-09-09 19:39:18 +0100

    Version 1.3 Updated copyright

--- gpg_vault/file.py
+++ gpg_vault/file.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2017 Paul Taylor
+# Copyright (c) 2017, 2018 Paul Taylor
 #
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation files
